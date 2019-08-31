0a3460793edf45ee6aa9543a43be8f67c9cff4e4
Benjamin Weigt <benweigt@gmail.com>
2019-09-01 03:59:06 +1000

    fix to queryselector target

--- postir/resources/views/users.blade.php
+++ postir/resources/views/users.blade.php
@@ -10,7 +10,7 @@
 		@endforeach
 	</div>
 	<script>
-		let users = document.querySelectorAll('.post');
+		let users = document.querySelectorAll('.user');
 		for (const user of users) {
 			user.addEventListener('click', ()=>{
 				location.href = '{{REL_DIR}}/users/'+user.dataset.username;
