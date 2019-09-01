b0142ffb6b47037a0588441ab348070a1ff71845
Benjamin Weigt <benweigt@gmail.com>
2019-09-01 17:17:42 +1000

    styling started

--- postir/resources/views/designs/postir.blade.php
+++ postir/resources/views/designs/postir.blade.php
@@ -7,10 +7,11 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <title>Postir</title>
+		<link rel="stylesheet" type="text/css" href="{{asset('css/postir.css')}}">
+		<title>Postir</title>
   </head>
 	<body>
-		<nav class="navbar navbar-expand-sm navbar-light bg-light">
+		<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
 			<a class="navbar-brand" href="{{REL_DIR}}"><span class="emoji-anim">
 				@foreach (EMOJ_LIST as $emoji)
 					<span class="emoji-anim-emoji">{{$emoji}}</span>
